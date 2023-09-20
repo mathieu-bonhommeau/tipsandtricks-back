f29b8fe5f5b8b94935590ef517c33ec3d8d13205
Mathieu Bonhommeau <m.bonhommeau@unlimitedpix.com>
2023-09-20 18:16:30 +0200

    TAT-53-migration
    - add dependency container system with test
    - add pretty-lint command in package.json
    - add job pretty-lint in github action

--- src/_config/db/init-db.ts
+++ src/_config/db/init-db.ts
@@ -1,30 +1,29 @@
-import postgres, {Sql} from 'postgres'
+import postgres, { Sql } from 'postgres'
 import * as dotenv from 'dotenv'
-import * as process from "process";
-import * as fs from "fs";
-import * as path from "path";
+import * as process from 'process'
+import * as fs from 'fs'
+import * as path from 'path'
 dotenv.config()
-
 
 export class InitDb {
     private _pg: Sql
 
     get pg(): postgres.Sql {
-        return this._pg;
+        return this._pg
     }
 
     async init(): Promise<void> {
         this._pg = postgres({
-            host                 : process.env.PGHOST || '127.0.0.1',            // Postgres ip address[s] or domain name[s]
-            port                 : process.env.PGPORT ? parseInt(process.env.PGPORT) : 5433,          // Postgres server port[s]
-            database             : process.env.PGDB || 'tipsandtricks',            // Name of database to connect to
-            username             : process.env.PGUSER || 'ttuser',            // Username of database user
-            password             : process.env.PGPASSWORD || 'changeme',            // Username of database user
+            host: process.env.PGHOST || '127.0.0.1', // Postgres ip address[s] or domain name[s]
+            port: process.env.PGPORT ? parseInt(process.env.PGPORT) : 5433, // Postgres server port[s]
+            database: process.env.PGDB || 'tipsandtricks', // Name of database to connect to
+            username: process.env.PGUSER || 'ttuser', // Username of database user
+            password: process.env.PGPASSWORD || 'changeme', // Username of database user
         })
     }
 
     async readFiles(): Promise<void> {
-        const sqlFiles: {schema: String[]} = {
+        const sqlFiles: { schema: string[] } = {
             schema: fs.readdirSync(path.join(__dirname, './migrations/schema')),
         }
 
@@ -33,14 +32,14 @@ export class InitDb {
         }
     }
 
-    async playSqlQueries(type: String ,files: String[]): Promise<void> {
+    async playSqlQueries(type: string, files: string[]): Promise<void> {
         for (const file of files) {
             await this._pg.file(path.join(__dirname, `./migrations/${type}/${file}`))
         }
     }
 
     async clearDb() {
-        const __dirname = path.dirname(__filename);
+        const __dirname = path.dirname(__filename)
         return this._pg.file(path.join(__dirname, `./migrations/drop_db.sql`))
     }
 }
