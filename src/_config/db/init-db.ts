f8520dfb7572e2df0fa70b95f057dcde20f11158
Mathieu Bonhommeau <m.bonhommeau@unlimitedpix.com>
2023-09-21 10:21:31 +0200

    follow rebase

--- src/_config/db/init-db.ts
+++ src/_config/db/init-db.ts
@@ -1,15 +1,15 @@
-import postgres, { Sql } from 'postgres'
-import * as dotenv from 'dotenv'
-import * as process from 'process'
-import * as fs from 'fs'
-import * as path from 'path'
-dotenv.config()
+import postgres, { Sql } from 'postgres';
+import * as dotenv from 'dotenv';
+import * as process from 'process';
+import * as fs from 'fs';
+import * as path from 'path';
+dotenv.config();
 
 export class InitDb {
-    private _pg: Sql
+    private _pg: Sql;
 
     get pg(): postgres.Sql {
-        return this._pg
+        return this._pg;
     }
 
     async init(): Promise<void> {
@@ -19,35 +19,35 @@ export class InitDb {
             database: process.env.PGDB || 'tipsandtricks', // Name of database to connect to
             username: process.env.PGUSER || 'ttuser', // Username of database user
             password: process.env.PGPASSWORD || 'changeme', // Username of database user
-        })
+        });
     }
 
     async readFiles(): Promise<void> {
         const sqlFiles: { schema: string[] } = {
             schema: fs.readdirSync(path.join(__dirname, './migrations/schema')),
-        }
+        };
 
         for (const [type, files] of Object.entries(sqlFiles)) {
-            await this.playSqlQueries(type, files)
+            await this.playSqlQueries(type, files);
         }
     }
 
     async playSqlQueries(type: string, files: string[]): Promise<void> {
         for (const file of files) {
-            await this._pg.file(path.join(__dirname, `./migrations/${type}/${file}`))
+            await this._pg.file(path.join(__dirname, `./migrations/${type}/${file}`));
         }
     }
 
     async clearDb() {
-        const __dirname = path.dirname(__filename)
-        return this._pg.file(path.join(__dirname, `./migrations/drop_db.sql`))
+        const __dirname = path.dirname(__filename);
+        return this._pg.file(path.join(__dirname, `./migrations/drop_db.sql`));
     }
 }
 
 (async () => {
-    const init = new InitDb()
-    await init.init()
-    await init.clearDb()
-    await init.readFiles()
-    await init.pg.end()
-})()
+    const init = new InitDb();
+    await init.init();
+    await init.clearDb();
+    await init.readFiles();
+    await init.pg.end();
+})();
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const app = express()
-const port = process.env.NODE_PORT
+const app = express();
+const port = process.env.NODE_PORT;
 app.get('/', (_, res) => {
-    res.send('Express + TypeScript Server')
-})
+    res.send('Express + TypeScript Server');
+});
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`)
-})
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
